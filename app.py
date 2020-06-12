dec81c99383772a0163b6f504152a605f6353f4f
Pragyan Varshney <pragyanvarshney@gmail.com>
2020-06-12 19:36:16 +0530

    Adding mysql to application

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
 from flask import Flask, render_template, url_for, redirect
 from flask_bootstrap import Bootstrap
-from flask_mysql import MySQL
+from flask_mysqldb import MySQL
+import yaml
+
 app = Flask(__name__)
 Bootstrap(app)
 
+# Configure db
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+mysql = MySQL(app)
 
 @app.route('/')
 def index() :
-    fruits = ['Apple', 'Banana', 'Orange']
-    return render_template('index.html',fruits=fruits)
+    cur = mysql.connection.cursor()
+    # cur.execute("INSERT INTO user VALUES(%s)",['MIKE'])
+    # mysql.connection.commit()
+    # fruits = ['Apple', 'Banana', 'Orange']
+    # return render_template('index.html',fruits=fruits)
     # return redirect(url_for('about'))
+    result = cur.execute("SELECT * FROM user")
+    if result>0:
+        users = cur.fetchall()
+        print(users)
+    return render_template('index.html')
 
 @app.route('/about')
 def about() :
