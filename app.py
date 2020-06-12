02b2d8a005ccd8649653dad2ec76a2378b583e8e
Pragyan Varshney <pragyanvarshney@gmail.com>
2020-06-12 17:40:24 +0530

    Before adding MYSQL

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template, url_for, redirect
+from flask_bootstrap import Bootstrap
+from flask_mysql import MySQL
 app = Flask(__name__)
+Bootstrap(app)
+
 
 @app.route('/')
 def index() :
-    # return render_template('index.html')
-    return redirect(url_for('about'))
+    fruits = ['Apple', 'Banana', 'Orange']
+    return render_template('index.html',fruits=fruits)
+    # return redirect(url_for('about'))
 
 @app.route('/about')
 def about() :
     return render_template('about.html')
+
+@app.route('/css')
+def css():
+    return render_template('css.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
